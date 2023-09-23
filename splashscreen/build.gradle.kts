759cae1ba901bb534067b5b3eafd425a0b951acc
MrRuslanYT <pandacorpglobal@gmail.com>
2023-09-23 14:32:00 +0300

    Added maven publishing

--- splashscreen/build.gradle.kts
+++ splashscreen/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -31,4 +32,18 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.MrRuslanYT"
+            artifactId = "SplashScreen"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
